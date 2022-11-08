72dbe2006efd6e23db6c5f6959fcfd89e159bc93
Inessa Rotnova <inessarotnova@Inessas-MacBook-Pro.local>
2022-11-08 21:06:25 +0200

    Task 5 - Change api function names

--- front-project/src/api/auth.api.js
+++ front-project/src/api/auth.api.js
@@ -1,27 +1,24 @@
 import fetchDB from "../utils/api";
 
-export function postSignIn({ email, password }) {
-  console.log(email, password);
-  return fetchDB("login", {
-    method: "POST",
-    mode: "no-cors",
-    body: {
-      email: email,
-      password: password,
-    },
-  });
-}
+export const authApi = {
+  signIn({ email, password }) {
+    return fetchDB("login", {
+      method: "POST",
+      body: {
+        email: email,
+        password: password,
+      },
+    });
+  },
 
-export function postSignUp({ username, email, password }) {
-  console.log(username, email, password);
-
-  return fetchDB("register", {
-    method: "POST",
-    mode: "no-cors",
-    body: {
-      username: username,
-      email: email,
-      password: password,
-    },
-  });
-}
+  signUp({ username, email, password }) {
+    return fetchDB("register", {
+      method: "POST",
+      body: {
+        username: username,
+        email: email,
+        password: password,
+      },
+    });
+  },
+};
